2def0bdc09ee2d6cf365a877d0acac9367c42dd5
Incrediblybadname <116849506+Incrediblybadname@users.noreply.github.com>
2023-01-12 18:20:56 -0600

    last one

--- src/main/java/frc/robot/commands/runMotor.java
+++ src/main/java/frc/robot/commands/runMotor.java
@@ -31,7 +31,7 @@ public class runMotor extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-      m_motor.move(0.5);
+      m_motor.move(0.05);
   }
 
   // Called once the command ends or is interrupted.
